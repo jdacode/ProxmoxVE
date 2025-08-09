b1d523e2e4325d1d5d5bc4aed4c0869a439afbe2
jdacode <49216638+jdacode@users.noreply.github.com>
2025-08-09 12:40:23 +0930

    test path

--- install/comfyui-install.sh
+++ install/comfyui-install.sh
@@ -25,9 +25,11 @@ msg_info "Setup ${APPLICATION}"
 RELEASE=$(curl -fsSL https://api.github.com/repos/comfyanonymous/ComfyUI/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 curl -fsSL -o "${APPLICATION}.zip" "https://github.com/comfyanonymous/ComfyUI/archive/refs/tags/${RELEASE}.zip"
 unzip -q "${APPLICATION}.zip"
-ls -hals
+TEST=$(ls -hals)
+msg_info "${TEST}"
 mv "${APPLICATION}" "/opt/"
-ls -hals /opt/
+TEST=$(ls -hals /opt/)
+msg_info "${TEST}"
 $STD uv venv "/opt/${APPLICATION}/venv"
 $STD uv pip install -r "/opt/${APPLICATION}/requirements.txt" --python="/opt/${APPLICATION}/venv/bin/python"
 #
